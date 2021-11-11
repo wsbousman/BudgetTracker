3df1d199163d3781d4b03b5e54d6f054f07bbba3
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-11-11 00:11:49 -0600

    Service Worker
    Service worker installing but not caching files. Investigating.

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = 'BudgetTracker';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/idb.js",
+    "./js/index.js",
+    "../dist/main.bundle.js"
+  ];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
