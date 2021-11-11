bdc9e376b90f911dc0f89f8d27107fbfe0df4909
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-11-11 01:42:48 -0600

    Manifest
    Webpack manifest created, icons functional

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-const path = require("path");
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require('path');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 
-module.exports = {
+const config = {
 
     entry: './assets/js/script.js',
 
@@ -9,7 +13,32 @@ module.exports = {
         filename: 'main.bundle.js'
       },
 
+    plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery'
+        }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static'
+        }),
+        new WebpackPwaManifest({
+            name: "Budget Tracker",
+            short_name: "Budget",
+            description: "An app for avoiding overdraft fees",
+            background_color: "#33ACFF",
+            theme_color: "#B910FD",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+            src: path.resolve("./public/icons/icon-192x192.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+            }]
+        })
+      ],
+
     mode: 'development'
 
 };
 
+module.exports = config
